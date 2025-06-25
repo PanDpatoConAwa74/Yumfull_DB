cce4ada447846ef19c4f33516f9568f46d8c8ba7
LAPT-CSH-03 <tomasedubonilla@gmail.com>
2025-06-25 14:38:59 -0600

    Se creo la estructura del proyecto

---
+++ core/models/__init__.py
@@ -0,0 +1,7 @@
+from .users import User
+from .gamification import Level, AchievementType, UserAchievement, Challenge, UserChallengeProgress
+from .health import SmartWatchData, UserGoal, MedicalHistory, MenstrualPeriod, HealthTip
+from .nutrition import NutritionalCategory, Ingredient, NutritionalProperty, Recipe, RecipeIngredient
+from .shopping import ShoppingList, ShoppingListItem, ShoppingCart, CartItem
+from .ecommerce import Store, StoreProduct, PriceHistory
+from .scraping import ScrapingConfig, ScrapingLog
